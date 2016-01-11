c54ae8229a17ef808023b282faa46329b2f14167
Eloy Durán <eloy.de.enige@gmail.com>
2016-01-11 14:09:07 +0100

    Implement scheduling in a timeframe.

--- lib/time_zone_scheduler.rb
+++ lib/time_zone_scheduler.rb
@@ -19,4 +19,32 @@ class TimeZoneScheduler
     end
     system_time
   end
+
+  def schedule_in_timeframe(from, timeframe)
+    time = time_in_local_timeframe(from, timeframe, true)
+    time.in_time_zone(Time.zone)
+  end
+
+  private
+
+  def time_in_local_timeframe(reference, timeframe, return_time)
+    time = reference.in_time_zone(@time_zone)
+    date = time.strftime('%F')
+    not_before = @time_zone.parse("#{date} #{timeframe.min}")
+    if time < not_before
+      result = :lt
+    else
+      not_after = @time_zone.parse("#{date} #{timeframe.max}")
+      result = time > not_after ? :gt : :eq
+    end
+    if return_time
+      case result
+      when :lt then not_before
+      when :gt then not_before.tomorrow
+      when :eq then time
+      end
+    else
+      result == :eq
+    end
+  end
 end
--- test/time_zone_scheduler_test.rb
+++ test/time_zone_scheduler_test.rb
@@ -24,10 +24,6 @@ describe TimeZoneScheduler do
   end
 
   describe "#schedule_on_date" do
-    before do
-      @at = ActiveSupport::TimeZone['UTC'].parse('2015-10-25 10:00')
-    end
-
     {
       'Pacific/Kiritimati' => 8,
         'Europe/Amsterdam' => 21, # +1 for DST change
@@ -37,8 +33,10 @@ describe TimeZoneScheduler do
             'Pacific/Niue' => 33,
     }.each do |time_zone, hours_from_now|
       it "schedules the date+time in `#{time_zone}' to be #{hours_from_now} hours from now" do
+        at = ActiveSupport::TimeZone['UTC'].parse('2015-10-25 10:00')
+
         scheduler = TimeZoneScheduler.new(time_zone)
-        scheduled_time = scheduler.schedule_on_date(@at)
+        scheduled_time = scheduler.schedule_on_date(at)
 
         scheduled_time.must_equal hours_from_now.hours.from_now
 
@@ -55,6 +53,37 @@ describe TimeZoneScheduler do
       lambda do
         TimeZoneScheduler.new('Pacific/Kiritimati').schedule_on_date(at)
       end.must_raise(ArgumentError)
+    end
+  end
+
+  describe "#schedule_in_timeframe" do
+    before do
+      # Reference time in UTC: 2015-10-25 12:00
+      @from = ActiveSupport::TimeZone['Brazil/East'].parse('2015-10-25 10:00')
+      @timeframe = '08:00'..'14:00'
+      @real_time = 24.hours.from_now
+    end
+
+    it 'delivers in real-time where local time falls within the allowed timeframe' do
+      # This is in the reference timezone, so no changes required.
+      TimeZoneScheduler.new('Brazil/East').schedule_in_timeframe(@from, @timeframe).must_equal(@real_time)
+      # This is 2015-10-25 08:00 (America/New_York).
+      TimeZoneScheduler.new('America/New_York').schedule_in_timeframe(@from, @timeframe).must_equal(@real_time)
+      # This is 2015-10-25 15:00 (Europe/Amsterdam) with DST, but as during
+      # this night DST ended, it is actually 2015-10-25 14:00.
+      TimeZoneScheduler.new('Europe/Amsterdam').schedule_in_timeframe(@from, @timeframe).must_equal(@real_time)
+    end
+
+    it 'delays those that fall before the allowed timeframe till start same day' do
+      # This is 2015-10-26 02:00 Pacific/Kiritimati.
+      TimeZoneScheduler.new('Pacific/Kiritimati').schedule_in_timeframe(@from, @timeframe).must_equal(@real_time + 6.hours)
+      # This is 2015-10-25 01:00 Pacific/Niue.
+      TimeZoneScheduler.new('Pacific/Niue').schedule_in_timeframe(@from, @timeframe).must_equal(@real_time + 7.hours)
+    end
+
+    it 'delays those that fall after the allowed timeframe till start next day' do
+      # This is 2015-10-25 15:00 Europe/Moscow.
+      TimeZoneScheduler.new('Europe/Moscow').schedule_in_timeframe(@from, @timeframe).must_equal(@real_time + 17.hours)
     end
   end
 end
